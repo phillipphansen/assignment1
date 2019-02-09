b6adb1b164a27a2d003b2f2bc7a2af8095fcb958
Phil Hansen <philliphansen@unomaha.edu>
2019-02-09 10:17:48 -0600

    Updated settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6u)gc^@32@e@3lon$=tx$h8)w@ahet37t2ah^-purf$t4v*k%d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
